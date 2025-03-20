f94a6074f6231725ff2d4507bf11acdcbcd922d2
SamuelAbiodun-dev <samuelabiodun324@gmail.com>
2025-03-20 18:52:28 +0100

    feature 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Router, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import VideoRoom from "./pages/VideoRoom";
 
 function App(){
   return (
-   <Routes>
+    <Router>
+      <Routes>
     <Route path='/' element={<Home/>}/>
     <Route path='/room/:roomId' element={<VideoRoom/>}/>
    </Routes>
+    </Router>
+      
+   
   )
 }
 
